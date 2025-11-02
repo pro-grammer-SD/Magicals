e792a8c16af600db344a2a22d6b710b47637110a
pro-grammer-SD <geniussantu1983@gmail.com>
2025-11-02 11:04:38 +0530

    up

--- app.py
+++ app.py
@@ -2,5 +2,4 @@ import streamlit as st
 st.set_page_config(page_title="magicals", layout="wide")
 st.title("magicals")
 st.write("use the sidebar to navigate")
-with st.sidebar:
-    st.button("discover")
+st.sidebar()
